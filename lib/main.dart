3506e42c25009ade77905e9b3aff0cfdb8e82bc7
Kristian Grønås <Kristian.Gronas@student.uib.no>
2022-09-28 18:47:19 +0200

    Make a basic navigation button for prototyping

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dichotic/test.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -48,18 +49,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var pageroute = MaterialPageRoute(builder: (context) => const TestApp(title: "titlete"));
 
   @override
   Widget build(BuildContext context) {
@@ -96,19 +86,17 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'You have pushed',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.push(context, pageroute);
+        },
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.next_plan),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
